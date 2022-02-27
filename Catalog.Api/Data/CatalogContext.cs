846e45401fe8eec188187bdfc7d341cbde7cffea
diogo-soares <diogosoaresk8@hotmail.com>
2022-02-27 13:37:25 -0300

    Add Mongo-Drive

---
+++ Catalog.Api/Data/CatalogContext.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.Api.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.Api.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var cliente = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var database = new MongoClient(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            var product = new MongoClient(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get;  }
+    }
+}
---
+++ Catalog.Api/Data/CatalogContextSeed.cs
@@ -0,0 +1,31 @@
+﻿using Catalog.Api.Entities;
+using MongoDB.Driver;
+using System.Collections.Generic;
+
+namespace Catalog.Api.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetMyProducts());
+            }
+        }
+        public static IEnumerable<Product> GetMyProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Id = "6065165",
+                    Name = "IPhone X",
+                    Description = "Nice"
+                }
+            };
+        }
+    }
+}
