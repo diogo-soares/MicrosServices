802268eaa472439edf277a3ba734b5fd9c77b30d
diogo-soares <diogosoaresk8@hotmail.com>
2022-02-27 14:10:06 -0300

    Implementation Repository

---
+++ Catalog.Api/Repository/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.Api.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.Api.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProducts(string id);
+        Task<IEnumerable<Product>> GetProductsByName(string name);
+        Task<IEnumerable<Product>> GetProductsByCategory(string categoryName);
+
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+    }
+}
